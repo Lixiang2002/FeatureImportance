e4b0f38c80b5f15e5494ea06e1339545a99d00ab
Li Xiang <lxlim2002@gmail.com>
2023-05-15 10:55:21 +0800

    added test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,4 +11,11 @@ using Flux, LinearAlgebra
     res = importance(model,lossfn,X,target=Y)
     @test length(res) == 4
     @test typeof(res) == Vector{Float32}
+
+    m = 3; n = 4
+    fm = vcat(reshape([j^k for k = 1:m for j = 1:n], (n,m))',[l^2+l for l = 1:n]')
+    model1 = Dense(3 => 1)
+    model1.weight .= [1.01,0.99,0.01]' 
+    res1 = importance(model1, lossfn, fm[1:3,:], target = fm[4,:]')
+    @test res1[2] > res1[1] > res1[3]
 end
